f8648a85a8290fc3d148ceda3f0486b7011b8337
keindev <kein@tagproject.ru>
2021-03-05 01:13:42 +0300

    test: refactor tests

--- src/__tests__/Task.test.ts
+++ src/__tests__/Task.test.ts
@@ -1,178 +1,176 @@
+import { Terminal } from 'stdout-update/lib/Terminal';
 import stripAnsi from 'strip-ansi';
-import { Terminal } from 'stdout-update/lib/terminal';
+
+import { IProgressBarOptions } from '../ProgressBar';
 import { Task, TaskStatus } from '../Task';
 import { TaskTree } from '../TaskTree';
 import { Theme, ThemeOptions } from '../Theme';
-import { IProgressBarOptions } from '../ProgressBar';
 
 const title = 'task';
 const themeOptions: ThemeOptions = { success: { symbol: '+' }, subtask: { symbol: '--' } };
-const progressBarOptions: IProgressBarOptions = { complete: '*', incomplete: '_' };
+const progressBarOptions: IProgressBarOptions = { completeChar: '*', incompleteChar: '_' };
 
 describe('Task', (): void => {
-    beforeAll((): void => {
-        TaskTree.tree().start({ silent: true });
+  beforeAll((): void => {
+    TaskTree.tree().start({ silent: true });
+  });
+
+  it('Default', (): void => {
+    const task = new Task(title).complete();
+
+    expect(task.isPending).toBeFalsy();
+    expect(task.text).toBe(title);
+    expect(task.status).toBe(TaskStatus.Completed);
+  });
+
+  it('Update', (): void => {
+    const task = new Task(title).update('new title');
+
+    expect(task.text).toBe('new title');
+  });
+
+  it('Check for errors or warnings in the task', (): void => {
+    const task = new Task(title);
+    const subtask = task.add(title);
+
+    subtask.warn('warning');
+    subtask.error('error');
+
+    expect(task.haveWarnings).toBeTruthy();
+    expect(task.haveErrors).toBeTruthy();
+  });
+
+  describe('Correct status changing', (): void => {
+    let task: Task;
+
+    beforeEach((): void => {
+      task = new Task('pending task');
     });
 
-    it('Default', (): void => {
-        const task = new Task(title).complete();
+    it('Completed', (): void => {
+      task.complete(title);
 
-        expect(task.isPending()).toBeFalsy();
-        expect(task.getText()).toBe(title);
-        expect(task.getStatus()).toBe(TaskStatus.Completed);
+      expect(task.status).toBe(TaskStatus.Completed);
+      expect(task.isPending).toBeFalsy();
+      expect(task.text).toBe(title);
     });
 
-    it('Update', (): void => {
-        const task = new Task(title).update('new title');
+    it('Skipped', (): void => {
+      task.skip(title);
 
-        expect(task.getText()).toBe('new title');
+      expect(task.status).toBe(TaskStatus.Skipped);
+      expect(task.isPending).toBeFalsy();
+      expect(task.text).toBe(title);
     });
 
-    it('Check for errors or warnings in the task', (): void => {
-        const task = new Task(title);
-        const subtask = task.add(title);
+    it('Failed', (): void => {
+      let isThrowException = false;
 
-        subtask.warn('warning');
-        subtask.error('error');
+      try {
+        task.fail(title);
+      } catch {
+        isThrowException = true;
+      } finally {
+        expect(isThrowException).toBeTruthy();
+        expect(task.status).toBe(TaskStatus.Failed);
+        expect(task.isPending).toBeFalsy();
+        expect(task.text).toBe(title);
+      }
+    });
+  });
 
-        expect(task.haveWarnings()).toBeTruthy();
-        expect(task.haveErrors()).toBeTruthy();
+  describe('Incorrect status changes', (): void => {
+    let tasks: Task[];
+
+    beforeEach((): void => {
+      tasks = [
+        new Task(title, { status: TaskStatus.Completed }),
+        new Task(title, { status: TaskStatus.Skipped }),
+        new Task(title, { status: TaskStatus.Failed }),
+      ];
     });
 
-    describe('Correct status changing', (): void => {
-        let task: Task;
+    it('Complete', (): void => {
+      tasks.forEach((item): void => {
+        item.complete();
 
-        beforeEach((): void => {
-            task = new Task('pending task');
-        });
-
-        it('Completed', (): void => {
-            task.complete(title);
-
-            expect(task.getStatus()).toBe(TaskStatus.Completed);
-            expect(task.isPending()).toBeFalsy();
-            expect(task.getText()).toBe(title);
-        });
-
-        it('Skipped', (): void => {
-            task.skip(title);
-
-            expect(task.getStatus()).toBe(TaskStatus.Skipped);
-            expect(task.isPending()).toBeFalsy();
-            expect(task.getText()).toBe(title);
-        });
-
-        it('Failed', (): void => {
-            try {
-                task.fail(title);
-            } catch {
-                // eslint-disable-next-line jest/no-try-expect
-                expect(task.getStatus()).toBe(TaskStatus.Failed);
-                // eslint-disable-next-line jest/no-try-expect
-                expect(task.isPending()).toBeFalsy();
-                // eslint-disable-next-line jest/no-try-expect
-                expect(task.getText()).toBe(title);
-            }
-        });
+        expect(item.status).toBe(TaskStatus.Failed);
+        expect(item.isPending).toBeFalsy();
+      });
     });
 
-    describe('Incorrect status changes', (): void => {
-        let tasks: Task[];
+    it('Skip', (): void => {
+      tasks.forEach((item): void => {
+        item.skip();
 
-        beforeEach((): void => {
-            tasks = [
-                new Task(title, { status: TaskStatus.Completed }),
-                new Task(title, { status: TaskStatus.Skipped }),
-                new Task(title, { status: TaskStatus.Failed }),
-            ];
-        });
-
-        it('Complete', (): void => {
-            tasks.forEach((item): void => {
-                try {
-                    item.complete();
-                } catch {
-                    // eslint-disable-next-line jest/no-try-expect
-                    expect(item.getStatus()).toBe(TaskStatus.Failed);
-                    // eslint-disable-next-line jest/no-try-expect
-                    expect(item.isPending()).toBeFalsy();
-                }
-            });
-        });
-
-        it('Skip', (): void => {
-            tasks.forEach((item): void => {
-                try {
-                    item.skip();
-                } catch {
-                    // eslint-disable-next-line jest/no-try-expect
-                    expect(item.getStatus()).toBe(TaskStatus.Failed);
-                    // eslint-disable-next-line jest/no-try-expect
-                    expect(item.isPending()).toBeFalsy();
-                }
-            });
-        });
-
-        it('Fail', (): void => {
-            tasks.forEach((item): void => {
-                try {
-                    item.fail();
-                } catch {
-                    // eslint-disable-next-line jest/no-try-expect
-                    expect(item.getStatus()).toBe(TaskStatus.Failed);
-                    // eslint-disable-next-line jest/no-try-expect
-                    expect(item.isPending()).toBeFalsy();
-                }
-            });
-        });
+        expect(item.status).toBe(TaskStatus.Failed);
+        expect(item.isPending).toBeFalsy();
+      });
     });
 
-    describe('Subtasks', (): void => {
-        it('Add to Pending task', (): void => {
-            const task = new Task(title);
+    it('Fail', (): void => {
+      tasks.forEach((item): void => {
+        let isThrowException = false;
 
-            expect(task.getActive()).toStrictEqual(task);
-            expect(task.getActive().id()).toBe(task.id());
+        try {
+          item.fail();
+        } catch {
+          isThrowException = true;
+        } finally {
+          expect(isThrowException).toBeTruthy();
+          expect(item.status).toBe(TaskStatus.Failed);
+          expect(item.isPending).toBeFalsy();
+        }
+      });
+    });
+  });
 
-            const subtask = task.add(title);
+  describe('Subtasks', (): void => {
+    it('Add to Pending task', (): void => {
+      const task = new Task(title);
 
-            expect(subtask.isPending()).toBeTruthy();
-            expect(task.getActive()).toStrictEqual(subtask);
-        });
+      expect(task.activeSubtask).toStrictEqual(task);
+      expect(task.activeSubtask.id).toBe(task.id);
 
-        it('Add to Completed task', (): void => {
-            const task = new Task(title).complete();
-            let subtask: Task | undefined;
+      const subtask = task.add(title);
 
-            try {
-                subtask = task.add(title);
-            } catch {
-                // eslint-disable-next-line jest/no-try-expect
-                expect(subtask).toBeUndefined();
-                // eslint-disable-next-line jest/no-try-expect
-                expect(task.getStatus()).toBe(TaskStatus.Failed);
-            }
-        });
-
-        it('Clear subtask', (): void => {
-            const task = new Task(title);
-
-            task.add(title).complete();
-            task.bar().complete();
-            task.complete().clear();
-
-            expect(task.getStatus()).toBe(TaskStatus.Completed);
-            expect(stripAnsi(task.render(new Theme(themeOptions)).join(Terminal.EOL))).toMatchSnapshot();
-        });
+      expect(subtask.isPending).toBeTruthy();
+      expect(task.activeSubtask).toStrictEqual(subtask);
     });
 
-    it('Progress bar', (): void => {
-        const task = new Task(title);
+    it('Add to Completed task', (): void => {
+      const task = new Task(title).complete();
+      let subtask: Task | undefined;
 
-        task.bar(':bar :percent :etas', progressBarOptions).complete();
-        task.complete();
-
-        expect(task.getStatus()).toBe(TaskStatus.Completed);
-        expect(stripAnsi(task.render(new Theme(themeOptions)).join(Terminal.EOL))).toMatchSnapshot();
+      try {
+        subtask = task.add(title);
+      } catch {
+        // empty
+      } finally {
+        expect(subtask).toBeUndefined();
+        expect(task.status).toBe(TaskStatus.Failed);
+      }
     });
+
+    it('Clear subtask', (): void => {
+      const task = new Task(title);
+
+      task.add(title).complete();
+      task.bar().complete();
+      task.complete().clear();
+
+      expect(task.status).toBe(TaskStatus.Completed);
+      expect(stripAnsi(task.render(new Theme(themeOptions)).join(Terminal.EOL))).toMatchSnapshot();
+    });
+  });
+
+  it('Progress bar', (): void => {
+    const task = new Task(title);
+
+    task.bar(':bar :percent :etas', progressBarOptions).complete();
+    task.complete();
+
+    expect(task.status).toBe(TaskStatus.Completed);
+    expect(stripAnsi(task.render(new Theme(themeOptions)).join(Terminal.EOL))).toMatchSnapshot();
+  });
 });
